48f4579a99217fcc0b314107fdc65c59531d8ce7
Gabriel Feitosa <gabriel.perino99@gmail.com>
2020-07-28 20:10:04 -0300

    updated article model, registred model,added articles tag

--- articles/models.py
+++ articles/models.py
@@ -9,3 +9,6 @@ class Article (models.Model):
     date= models.DateTimeField(auto_now_add=True)
     # add article thumbnail later
     # add author later
+
+    def __str__(self):
+        return self.title
