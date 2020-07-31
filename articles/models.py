c39f544b1cfab9a5d0860e6fa64767e398cc58e3
Gabriel Feitosa <gabriel.perino99@gmail.com>
2020-07-31 16:25:40 -0300

    created article user relationship

--- articles/models.py
+++ articles/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -8,6 +9,7 @@ class Article (models.Model):
     body= models.TextField()
     date= models.DateTimeField(auto_now_add=True)
     thumb= models.ImageField(default='default.png',blank=True)
+    author= models.ForeignKey(User,on_delete=models.CASCADE,default=None)
     # add author later
 
     def __str__(self):
